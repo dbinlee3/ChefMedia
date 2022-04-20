8bd4d9d3fcbb01b6aa8d6bc9d74d6c2538b6663e
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-04-19 21:18:12 -0700

    folder-and-file-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import LoginPage from './pages/login/Login.js'
+import SignupPage from './pages/signup/Signup.js'
+import Navbar from './components/navbar/Navbar.js'
 
 function App() {
   return (
-    <h1>ChefMedia</h1>
+    <>
+      <h1>ChefMedia</h1>
+      <LoginPage />
+      <SignupPage />
+      <Navbar />
+    </>
   );
 }
 
