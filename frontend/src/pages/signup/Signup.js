c16181afd5b6c7dd11168832b9f8334a5fb67da4
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-04-20 22:45:13 -0700

    Worked on Signup Page

--- frontend/src/pages/signup/Signup.js
+++ frontend/src/pages/signup/Signup.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import './Signup.css'
 
 function Signup() {
     return (
-        <div>
-            I am the Signup Page
-        </div>
+        <>
+            <div className="signupContainer">
+                <div className="signupWrapper">
+                    <div className="signupLeft">
+                        <h3 className="chefLogo">ChefMedia</h3>
+                        <span className="signupDescription">
+                            Connect with friends and the world of cooking, recipes and more.
+                        </span>
+                    </div> {/* end signupLeft */}
+
+                    <div className="signupRight">
+                        <div className="signupBox">
+                            <input placeHolder="Email" className="signupInput"/>
+                            <input placeHolder="Username" className="signupInput"/>
+                            <input placeHolder="Password" className="signupInput"/>
+                            <input placeHolder="Retype Password" className="signupInput"/>
+                            <a className="alreadyUser" href="./login">Already a user? Login!</a>
+                            <button className="signupButton">Sign up</button>
+                        </div> {/* end signupBox */}
+                    </div> {/* end signupRight */}
+                </div> {/* end signupWrapper */}
+            </div> {/* end signupContainer */}
+        </>
     )
 }
 
