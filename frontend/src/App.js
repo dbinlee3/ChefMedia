1204fc44c0b7895a1c1f4a7c97d1b17ec7385d0f
KennytheLordtheCoder <kennyinblue@gmail.com>
2022-05-04 16:01:17 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,6 @@ import Login from './pages/login/Login.js'
 import Signup from './pages/signup/Signup.js'
 import Home from './pages/home/Home.js'
 
-import Navbar from './components/navbar/Navbar.js'
-
 
 //Imported hooks
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
