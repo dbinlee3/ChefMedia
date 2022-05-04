cb00a53310d66a08523ccea6a41d652b10214c57
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-05-04 14:59:31 -0700

    login refresh

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -8,32 +8,6 @@ import { Link } from 'react-router-dom';
 function Login() {
     return (
         <>
-            <div className="loginContainer">
-
-                <video src={myVideo} autoPlay loop muted />
-
-                <h3 className="chefLogo">ChefMedia</h3>
-
-
-                <div className="usernameContainer">
-                    <label className="textField">Username: </label>
-                    <input type="text" />
-                </div>
-
-                <div className="passwordContainer">
-                    <label className="textField">Password: </label>
-                    <input type="text" />
-                </div>
-
-                <Link to="/">
-                    <button className="loginButton" type="button">
-                        Login
-                    </button>
-                </Link>
-
-
-            </div>
-
         </>
     )
 }
