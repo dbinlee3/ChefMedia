1bdaa1a9024774ea3565e244d6affd641994bf4f
Ben Lee <dbinlee3@gmail.com>
2022-07-28 15:11:38 -0700

    Added google auth

--- frontend/src/components/navbar/MenuItems.js
+++ frontend/src/components/navbar/MenuItems.js
@@ -13,11 +13,5 @@ export const MenuItems = [
         title: "Login",
         url: "/login",
         cName: "nav-links"
-    },
-    {
-        title: "Sign up",
-        url: "/signup",
-        cName: "nav-links"
     }
-
 ]
